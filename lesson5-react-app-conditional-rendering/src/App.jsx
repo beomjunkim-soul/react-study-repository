759ef058ed2601af7919aaaef1019a4f10b22917
beomjunkim-soul <kbg142337@naver.com>
2025-09-16 11:20:58 +0900

    feat:lesson5 login

--- lesson5-react-app-conditional-rendering/src/App.jsx
+++ lesson5-react-app-conditional-rendering/src/App.jsx
@@ -6,9 +6,8 @@ function App() {
     <div className="app">
       <h1>React 조건부 렌더링</h1>
       <p>상황에 따른 다른 화면 보여주기</p>
-      <LoginSystem/>
-    </div>
-    
+      <LoginSystem />
+    </div>    
   );
 }
 
